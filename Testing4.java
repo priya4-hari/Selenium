ea634adb218efba8fc0a40d71c2fd3564a0a3315
priya4-hari <haripriyaandavar2001@gmail.com>
2024-11-17 21:18:00 +0530

    Testing4

---
+++ Testing4.java
@@ -0,0 +1,28 @@
+package com.testing.org;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class Testing4 {
+	
+	public static void main(String[] args)
+	{
+		WebDriver driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demo.automationtesting.in/Frames.html");
+		driver.switchTo().frame("singleframe");
+		driver.findElement(By.xpath("(//div[@class='col-xs-6 col-xs-offset-5'])[1]/input")).sendKeys("WELCOME!");
+		driver.switchTo().parentFrame();
+		driver.findElement(By.xpath("(//a[@class='analystic'])[2]")).click();
+		WebElement ele1 = driver.findElement(By.xpath("(//iframe[@src='MultipleFrames.html'])"));
+		driver.switchTo().frame(ele1);
+		WebElement ele2 = driver.findElement(By.xpath("(//iframe[@src='SingleFrame.html'])[1]"));
+		driver.switchTo().frame(ele2);
+		driver.findElement(By.xpath("(//div[@class='col-xs-6 col-xs-offset-5'])[1]/input")).sendKeys("WELCOME WORLD!");
+		driver.switchTo().defaultContent();
+		driver.close();
+	}
+
+}
